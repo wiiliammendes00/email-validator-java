19d51efb0dc417d3042c0faf7abc5c61d30f6314
William Mendes <wiiliammendes00@gmail.com>
2026-05-14 16:23:04 -0300

    Adicionei validação básica de e-mail utilizando métodos da classe String

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
         String email = "wiiliammendes00@gmail.com";
 
         System.out.println("E-mail informado: " + email);
+
+        if (email.isBlank()) {
+            System.out.println("E-mail inválido!");
+        } else if (!email.contains("@")) {
+            System.out.println("E-mail precisa conter @");
+        } else if (!email.contains(".com")) {
+            System.out.println("E-mail precisa conter .com");
+        } else {
+            System.out.println("E-mail válido!");
+        }
     }
 }
