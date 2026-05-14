abe08ff74b540305c63f139ad32ae966652eee5e
William Mendes <wiiliammendes00@gmail.com>
2026-05-14 16:18:27 -0300

    Versão inicial do projeto de validação de e-mails

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String email = "wiiliammendes00@gmail.com";
+
+        System.out.println("E-mail informado: " + email);
+    }
+}
