720583ff3592ad1f4b3f8d2576bce87557964d0b
William Mendes <wiiliammendes00@gmail.com>
2026-05-15 16:41:25 -0300

    Implementei extração de usuário e domínio utilizando substring e indexOf

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,16 @@ public class Main {
         } else if (!email.contains(".com")) {
             System.out.println("E-mail precisa conter .com");
         } else {
+
+            int posicaoArroba = email.indexOf("@");
+
+            String usuario = email.substring(0, posicaoArroba);
+
+            String dominio = email.substring(posicaoArroba + 1);
+
             System.out.println("E-mail válido!");
+            System.out.println("Usuário: " + usuario);
+            System.out.println("Domínio: " + dominio);
         }
     }
 }
